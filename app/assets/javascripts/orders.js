31881838d796d92c4ed5ff655b2599b5bb5c2a2b
pavel <pavel@ubuntu.(none)>
2014-07-14 20:59:42 -0700

    create_input_quantity

---
+++ app/assets/javascripts/orders.js
@@ -0,0 +1,19 @@
+var gv_min = 10;
+var gv_max = 20;
+
+$(document).ready(function() {
+    $(function() {
+        $("#slider-range-min").slider({
+            range: "min",
+            value: gv_min,
+            min: 1,
+            max: gv_max,
+            slide: function( event, ui ) {
+                $( "#amount" ).val( ui.value );
+            }
+        });
+        $( "#amount" ).val( $( "#slider-range-min" ).slider( "value" ) );
+        $( "#slider_min_value" ).html( gv_min.toString() );
+        $( "#slider_max_value" ).html( gv_max.toString() );
+    });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715014606) do
+ActiveRecord::Schema.define(version: 20140715022255) do
 
   create_table "orders", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140715014606) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
 end
