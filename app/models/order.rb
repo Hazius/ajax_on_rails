b50f95be4595bf0fd63a96322ff7dd3efa9e98f2
pavel <pavel@ubuntu.(none)>
2014-07-14 22:19:51 -0700

    add_mailer

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ActiveRecord::Base
+
+	before_save { self.email = email.downcase }
+
+	validates :quantity, :name, :phone, :email, :address, 
+		presence: { massage: "Не все поля заполнены." }
+
+	validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
+		massage: "Email введен неверно." }
+
+	validates :phone, length: { is: 10,
+		massage: "Длина телефона должна быть равна 10 цифрам." }
+
 end
