491119b1ab096016235e7bc9ec9dfb1f0f5ee28f
pavel <pavel@ubuntu.(none)>
2014-07-14 22:53:28 -0700

    modify_mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,3 +1,9 @@
 class OrderMailer < ActionMailer::Base
-  default from: "from@example.com"
+  	default from: "from@example.com"
+
+  	def send_email(p_order)
+  		@order = p_order
+  		mail(to: p_order.email, subject: "Ваш заказ принят."
+  	end
+
 end
